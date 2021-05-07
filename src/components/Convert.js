96055111ae27bdfa565f16c78036b35ec0554270
builtbyjosh <pc.refuge@gmail.com>
2021-05-06 23:57:44 -0500

    added axios fetch for google translation

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,10 +1,32 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslate] = useState("");
+
   useEffect(() => {
-    console.log("new shit");
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
+        }
+      );
+      setTranslate(data.data.translations[0].translatedText);
+    };
+    doTranslation();
   }, [language, text]);
-  return <div></div>;
+
+  return (
+  <div>
+    <h1 className="ui header">{translated}</h1>
+  </div>
+  );
 };
 
 export default Convert;
